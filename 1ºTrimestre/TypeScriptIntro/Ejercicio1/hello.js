75ad9dd7bb1a0d33ed8419cd581fcc8882b28dc5
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2023-09-25 14:31:21 +0200

    Delete 1ºTrimestre/TypeScriptIntro/Ejercicio1 directory

--- 1ºTrimestre/TypeScriptIntro/Ejercicio1/hello.js
+++
@@ -1,6 +0,0 @@
-alert('Hola Mundo en TypeScript');
-function area(shape, width, height) {
-    var area = width * height;
-    return "I'm a " + shape + " with an area of " + area + " cm squared.";
-}
-document.body.innerHTML = area("rectangle", 30, 15);
--- 1ºTrimestre/TypeScriptIntro/Ejercicio1/interfaces.js
+++
@@ -1,6 +0,0 @@
-function area1(shape) {
-    var area = shape.width * shape.height;
-    return "El area del " + shape.name + " es de " + area + " cm cuadrados.";
-}
-console.log(area1({ name: 'rectangulo', width: 30, height: 15 }));
-console.log(area1({ name: 'triangulo', width: 10, height: 5, color: 'red' }));
